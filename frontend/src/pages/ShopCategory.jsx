b6bfef3d073f09f9350876bb3819da386030ebf9
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-02-14 11:57:32 +0530

    created contextapi

--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -1,12 +1,41 @@
 import React, { useContext } from "react";
 import "./CSS/ShopCategory.css";
 import all_product from "./../components/Assets/all_product";
+import dropdown_icon from "./../components/Assets/dropdown_icon.png";
+import Item from "../components/Item/Item.jsx";
 import { ShopContext } from "../context/ShopContex";
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="" />
+      <img className="shopcategory-banner" src={props.banner} alt="" />
+      <div className="shopcategory-indexsort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort By <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-product">
+        {all_product.map((item, index) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={index}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">Explore More</div>
     </div>
   );
 };
